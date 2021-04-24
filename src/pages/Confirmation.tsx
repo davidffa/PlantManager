c5fb49345eb1dada3404021e8f679dbb3c298f32
David Amorim <davidfilipeamorim@gmail.com>
2021-04-24 17:08:27 +0100

    Add PlantSelect screen

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
+
 import { Button } from '../components/Button';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
 export function Confirmation() {
+  const navigation = useNavigation();
+
+  function handleMoveOn() {
+    navigation.navigate('PlantSelect');
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
@@ -23,6 +31,7 @@ export function Confirmation() {
         <View style={styles.footer}>
           <Button 
             title="Começar"
+            onPress={handleMoveOn}
           />
         </View>
       </View>      
